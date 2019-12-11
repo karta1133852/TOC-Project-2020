642077332958cdc8c28b5f6d73683907a072dc2a
karta1133852 <tr30111@gmail.com>
2019-12-11 17:39:42 +0800

    Finish weapon search

--- fsm.py
+++ fsm.py
@@ -1,23 +1,24 @@
 from transitions.extensions import GraphMachine
 
-from utils import send_text_message
+from utils import *
 
 weapon_list = [
-        "大劍", "太刀", "片手劍", "雙劍",
-        "大錘", "狩獵笛", "長槍", "銃槍",
-        "斬擊斧", "操蟲棍", "充能斧", "輕弩", "重弩", "弓"
-    ]
-    
-    weapon_list_url = [
-        "great_swords", "long_sword", "sword_shield", "dual_blades",
-        "hammer", "hunting_horn", "lances", "gunlance",
-        "switch_ace", "insect_glaive", "charge_blade", "light_bowgun", "heavy_bowgun", "bow"
-    ]
-    
-    weapon_url = "https://www.mhchinese.wiki/weapons/c/"
-    
-    weapon_select_type = 0
+    "大劍", "太刀", "片手劍", "雙劍",
+    "大錘", "狩獵笛", "長槍", "銃槍",
+    "斬擊斧", "操蟲棍", "充能斧", "輕弩", "重弩", "弓"
+]
 
+weapon_list_url = [
+    "great_swords", "long_sword", "sword_shield", "dual_blades",
+    "hammer", "hunting_horn", "lances", "gunlance",
+    "switch_ace", "insect_glaive", "charge_blade", "light_bowgun", "heavy_bowgun", "bow"
+]
+
+weapon_cate_url = "https://www.mhchinese.wiki/weapons/c/"
+mhw_wiki_url = "https://www.mhchinese.wiki"
+
+weapon_select_type = 0
+current_url = ""
 
 class TocMachine(GraphMachine):
     
@@ -33,29 +34,44 @@ class TocMachine(GraphMachine):
 
         reply_token = event.reply_token
         send_text_message(reply_token, "請輸入武器類別")
-        # self.go_back()
-
-    def on_exit_weapon_cate(self):
-        print("Leaving weapon_cate")
-
+    
+    def is_going_back_previous(self, event):
+        text = event.message.text
+        return text.lower() == "返回"
+    
     def is_going_to_weapon_select(self, event):
         text = event.message.text
         result = False
         global weapon_list
-        for i in weapon_list:
-            if weapon_list[i] == text.lower():
+        for s in weapon_list:
+            if s == text.lower():
                 result = True;
                 global weapon_select_type
-                weapon_select_type = i
+                weapon_select_type = weapon_list.index(s)
         return result
 
     def on_enter_weapon_select(self, event):
         print("I'm entering weapon_select")
 
         reply_token = event.reply_token
-        send_text_message(reply_token, "請輸入" + weapon_list[weapon_select_type] + "名稱(可用'[]'代替'【】')")
-        # self.go_back()
-
-    def on_exit_weapon_select(self):
-        print("Leaving weapon_select")
-
+        send_text_message(reply_token, "請輸入 " + weapon_list[weapon_select_type] + " 的名稱(可用'[]'代替'【】')")
+    
+    def is_going_to_weapon_details(self, event):
+        text = event.message.text
+        global current_url
+        current_url = check_weapon_name(weapon_select_type, text)
+        if current_url == "":
+            reply_token = event.reply_token
+            send_text_message(reply_token, "輸入錯誤，請重新輸入！")
+            return False
+        else:
+            return True
+        
+    def on_enter_weapon_details(self, event):
+        print("I'm entering weapon_details")
+        
+        result = get_weapon_details(current_url)
+        reply_token = event.reply_token
+        send_text_message(reply_token, result)
+        
+        self.go_back()
--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ import os
 from linebot import LineBotApi, WebhookParser
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
+import requests
+from bs4 import BeautifulSoup
+
 
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
 
@@ -12,6 +15,53 @@ def send_text_message(reply_token, text):
     line_bot_api.reply_message(reply_token, TextSendMessage(text=text))
 
     return "OK"
+
+weapon_list_url = [
+    "great_swords", "long_sword", "sword_shield", "dual_blades",
+    "hammer", "hunting_horn", "lances", "gunlance",
+    "switch_ace", "insect_glaive", "charge_blade", "light_bowgun", "heavy_bowgun", "bow"
+]
+weapon_cate_url = "https://www.mhchinese.wiki/weapons/c/"
+mhw_wiki_url = "https://www.mhchinese.wiki"
+
+def check_weapon_name(weapon_type, name):
+    
+    name = name.upper()
+    name = name.replace("I", "Ⅰ")
+    name = name.replace("[", "【")
+    name = name.replace("]", "】")
+    name = name.replace("[", "［")
+    name = name.replace("]", "］")
+    r = requests.get(weapon_cate_url + weapon_list_url[weapon_type])
+    if r.status_code == requests.codes.ok:
+        soup = BeautifulSoup(r.text, "html.parser")
+        table = soup.find("table", {"class" : "simple-table"})
+        for div in table.findAll("div"): 
+            div.decompose()
+        for tag in table.findAll("a", {"class" : "weapon-link"}):
+            if name == tag.get_text().strip():
+                print(name)
+                return tag.get("href")
+    
+    return ""
+
+def get_weapon_details(url):
+
+    result = ""
+    url = mhw_wiki_url + url
+    r = requests.get(url)
+    if r.status_code == requests.codes.ok:
+        soup = BeautifulSoup(r.text, "html.parser")
+        table = soup.find("table", {"class" : "simple-table"})
+        td_tags = table.findAll("td")
+        result += "名稱：\t" + td_tags[0].string
+        result += "\n稀有度：\tRare" + td_tags[1].string
+        result += "\n攻擊：\t" + td_tags[3].string
+        result += "\n屬性：\t"
+        for span in td_tags[6].findAll("span"):
+            result += span.string + " "
+    
+    return result
 
 
 """
