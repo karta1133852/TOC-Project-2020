0a78e049ea45f021dac102083184e107b0f09e22
karta1133852 <tr30111@gmail.com>
2019-12-11 15:09:35 +0800

    Test 1

--- fsm.py
+++ fsm.py
@@ -4,33 +4,57 @@ from utils import send_text_message
 
 
 class TocMachine(GraphMachine):
+  
+    weapon_list = [
+        "大劍", "太刀", "片手劍", "雙劍",
+        "大錘", "狩獵笛", "長槍", "銃槍",
+        "斬擊斧", "操蟲棍", "充能斧", "輕弩", "重弩", "弓"
+    ]
+    
+    weapon_list_url = [
+        "great_swords", "long_sword", "sword_shield", "dual_blades",
+        "hammer", "hunting_horn", "lances", "gunlance",
+        "switch_ace", "insect_glaive", "charge_blade", "light_bowgun", "heavy_bowgun", "bow"
+    ]
+    
+    weapon_url = "https://www.mhchinese.wiki/weapons/c/"
+    
+    weapon_select_type = 0
+    
     def __init__(self, **machine_configs):
         self.machine = GraphMachine(model=self, **machine_configs)
 
-    def is_going_to_state1(self, event):
+    def is_going_to_weapon_cate(self, event):
         text = event.message.text
-        return text.lower() == "go to state1"
-
-    def is_going_to_state2(self, event):
-        text = event.message.text
-        return text.lower() == "go to state2"
-
-    def on_enter_state1(self, event):
-        print("I'm entering state1")
+        return text.lower() == "武器"
+    
+    def on_enter_weapon_cate(self, event):
+        print("I'm entering weapon_cate")
 
         reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state1")
-        self.go_back()
+        send_text_message(reply_token, "請輸入武器類別")
+        # self.go_back()
 
-    def on_exit_state1(self):
-        print("Leaving state1")
+    def on_exit_weapon_cate(self):
+        print("Leaving weapon_cate")
 
-    def on_enter_state2(self, event):
+    def is_going_to_weapon_select(self, event):
+        text = event.message.text
+        result = False
+        for i in weapon_list:
+            if weapon_list[i] == text.lower():
+                result = True;
+                global weapon_select_type
+                weapon_select_type = i
+        return result
+
+    def on_enter_weapon_select(self, event):
         print("I'm entering state2")
 
         reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state2")
-        self.go_back()
+        send_text_message(reply_token, "請輸入" + weapon_list[weapon_select_type] + "名稱(可用'[]'代替'【】')")
+        # self.go_back()
 
     def on_exit_state2(self):
         print("Leaving state2")
+
